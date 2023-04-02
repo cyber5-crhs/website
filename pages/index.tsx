fdae03b52a271a721eaa291b4a0fed0fc3a94b31
Joey Lent <supercolbat@protonmail.com>
2023-04-01 21:07:14 -0500

    Add header section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,49 @@
-import People from '@/components/People';
-import styles from '@/styles/Home.module.css';
+import { ReactNode } from "react";
+
 import {
+  Text,
   Flex, Spacer,
   ButtonGroup
 } from '@chakra-ui/react';
 
-export default function Home() {
+import styles from '@/styles/Home.module.scss';
+import People from '@/components/People';
+
+interface Props {
+  children: ReactNode;
+}
+
+function Red(props: Props) {
+  return <span style={{color: 'red'}}>
+    {props.children}
+  </span>;
+}
+
+function Header() {
   return (
-    <main className={styles.main}>
-      <Flex>
+    <header className={styles.header}>
+      <Flex minWidth='100%'>
+        <Text as='b' fontSize='4xl'>cyber<Red>5</Red></Text>
         <Spacer />
         <ButtonGroup>
           <a href="#">Mastodon</a>
           <a href="#">Matrix</a>
         </ButtonGroup>
       </Flex>
+      <Text>The <Red>Cybersecurity</Red> Club of Cinco Ranch High School</Text>
+      <Spacer />
+      <Text>
+        We a group of cybersecurity enthusiasts that practice <Red>ethical hacking</Red>,
+        host <Red>CTFs</Red>, and have fun together.
+      </Text>
+    </header>
+  );
+}
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Header />
       <People />
     </main>
   );
