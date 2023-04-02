d472fd25f5cea40f85a32dc9d6f2001d458c542f
Joey Lent <supercolbat@protonmail.com>
2023-04-02 13:37:53 -0500

    Fix typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,20 +14,20 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* Primary meta tags */}
         <title>cyber5 — The  Cybersecurity Club of CRHS</title>
         <meta name="title" content="cyber5 — The Cybersecurity Club of CRHS" />
-        <meta name="description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and have learn together." />
+        <meta name="description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and learn together." />
 
         {/* Open Graph / Facebook*/}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://cyber5.club/" />
         <meta property="og:title" content="cyber5 — The Cybersecurity Club of CRHS" />
-        <meta property="og:description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and have learn together." />
+        <meta property="og:description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and learn together." />
         <meta property="og:image" content="/assets/banner.png" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://cyber5.club/" />
         <meta property="twitter:title" content="cyber5 — The Cybersecurity Club of CRHS" />
-        <meta property="twitter:description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and have learn together." />
+        <meta property="twitter:description" content="We are a student-led club at Cinco Ranch High School made up of cybersecurity enthusiasts. We practice ethical hacking, host CTFs, and learn together." />
         <meta property="twitter:image" content="/assets/banner.png" />
       </Head>
       <ChakraProvider><Component {...pageProps} /></ChakraProvider>
