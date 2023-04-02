ed2b976a12e86b56f26dda6f787256c13a2ec2b9
Alexander Park <me@parkalex.dev>
2023-04-01 19:09:12 -0500

    fix stuff, delete goofy ah template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react/dist/chakra-provider';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
