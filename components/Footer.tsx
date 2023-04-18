785dbeb09bbff338bbc81abad3d627d39f33541f
Joey Lent <supercolbat@protonmail.com>
2023-04-17 20:24:25 -0500

    Mention non-profit status in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
   return (
     <Flex className={styles.main}>
       <Text fontSize="sm">cyber5 © 2023 | <Link href="/contact"><RedUnderline><span style={{color: '#FF002A'}}>contact</span></RedUnderline></Link></Text>
-      <Text fontSize="sm">23440 Cinco Ranch Blvd, Katy, TX 77494</Text>
+      <Text fontSize="sm">We are a <Link href="https://bank.hackclub.com/cyber5">501(c)(3) nonprofit</Link>, fiscally sponsored by <Link href="https://hackclub.com/bank">Hack Club</Link>.</Text>
     </Flex>
   );
 }
