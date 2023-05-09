2fa762feb2daf954769d91e6ba79a6c04438619f
Alexander Park <me@parkalex.dev>
2023-05-09 17:51:59 -0500

    register button change

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,9 +40,8 @@ function Header() {
         We a group of cybersecurity enthusiasts that practice <Red>ethical hacking</Red>,
         host <Red>CTFs</Red>, and have fun together.
       </Text>
-      <Text fontSize='18px' fontWeight='bold'>=============================</Text>
-     <Link href="/join"><Text fontSize='18px' fontWeight='bold'><u>{'>>> Register for the club <<<'}</u></Text></Link>
-     <Text fontSize='18px' fontWeight='bold'>=============================</Text>
+    
+     <Link href="/join"><Text fontSize='18px' fontWeight='bold'><u>{'>>> Register <<<'}</u></Text></Link>
     </header>
   );
 }
