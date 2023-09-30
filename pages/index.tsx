e6f7dcb79397c7b2d77b616b78dd7748e5e923a6
Alexander Park <me@parkalex.dev>
2023-09-30 18:11:27 -0500

    announcementn't

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ function Header() {
 
 export default function Home() {
   // Change this to set banner
-  const announcement = true; // enable the banner
+  const announcement = false; // enable the banner
   const number = 1; // meeting number
   const date = 'sept 27'; // meeting date
   const time = '14:50'; // meeting time
