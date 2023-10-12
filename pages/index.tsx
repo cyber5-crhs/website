149733841c58d5a95c4a70349208e201b8effd48
Alexander Park <me@parkalex.dev>
2023-10-11 21:01:54 -0500

    No more meeting 🚨🚨🚨🚨🚨🚨

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ function Header() {
 
 export default function Home() {
   // Change this to set banner
-  const announcement = true; // enable the banner
+  const announcement = false; // enable the banner
   const number = 2; // meeting number
   const date = 'oct 11'; // meeting date
   const time = '14:50'; // meeting time
